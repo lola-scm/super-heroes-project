93126beaa0ea0a92c48ce5c19da54a448f74769b
lola <lola@weddingplan.fr>
2025-11-21 10:37:18 +0100

    sauvegarde locale et formulaire

--- heroes.js
+++ heroes.js
@@ -2,7 +2,13 @@ fetch("heroes.json")
   .then(res => res.json())
   .then(jsonHeroes => {
     console.log("Héros chargés :", jsonHeroes);
-
     }
   );
+
+function storeHeroes(heroes) {
+  localStorage.setItem('heroes', JSON.stringify(heroes));
+}
+
+const heroes = JSON.parse(localStorage.getItem('heroes')) || [];
+console.log("Sauvegardé:" , heroes);
 
